12572cb6ee1a51e1d869b5c154bafbc8c2c215df
Yuriy Erokhin <yuriy.erokhin@deleteagency.com>
2020-10-20 14:21:52 +0500

    Added SocialLinks models and repository (wip)

---
+++ Shared/DeleteBoilerplate.Domain/Repositories/SocialLinkRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CMS.DocumentEngine.Types.DeleteBoilerplate;
+using CMS.Helpers;
+using CMS.SiteProvider;
+
+namespace DeleteBoilerplate.Domain.Repositories
+{
+    public interface ISocialLinkRepository
+    {
+        IEnumerable<SocialLink> GetSocialLinks(string siteName = null);
+    }
+
+    public class SocialLinkRepository : ISocialLinkRepository
+    {
+        private readonly string _projectCacheKey = "deleteboilerplate|sociallink";
+
+        public IEnumerable<SocialLink> GetSocialLinks(string siteName = null)
+        {
+            var socialLinks = new List<SocialLink>();
+
+            if (string.IsNullOrEmpty(siteName))
+            {
+                siteName = SiteContext.CurrentSiteName;
+            }
+
+            using (var cs = new CachedSection<List<SocialLink>>(ref socialLinks, CacheHelper.CacheMinutes(siteName), true, _projectCacheKey))
+            {
+                if (cs.LoadData)
+                {
+                    socialLinks = SocialLinkProvider.GetSocialLinks().OnSite(siteName).ToList();
+
+                    var cacheDependencies = new List<string>
+                    {
+                        $"nodes|{siteName}|{SocialLink.CLASS_NAME}|all"
+                    };
+
+                    cs.Data = socialLinks;
+                    cs.CacheDependency = CacheHelper.GetCacheDependency(cacheDependencies);
+                }
+            }
+
+            return socialLinks;
+        }
+    }
+}
